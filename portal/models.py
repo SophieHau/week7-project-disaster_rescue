105d549ff61fe421a2e1535d9a21173c9cb78a1d
Sophie <you@example.com>
2019-04-03 11:47:19 +0300

    creating Person model and populating fake data

--- portal/models.py
+++ portal/models.py
@@ -38,4 +38,20 @@ class PersonStatus(models.Model):
         return self.name
 
 
-# to do: add Person model
+class Person(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    other_name = models.CharField(max_length=100, null=True, default=None, blank=True)
+    status = models.ForeignKey(PersonStatus, on_delete=models.SET_NULL, blank=True, null=True)
+    id_number = models.IntegerField(null=True, default=None, blank=True, unique=True)
+    dob = models.DateField(null=True, default=None, blank=True)
+    mobile = models.CharField(max_length=20, null=True, default=None, blank=True)
+    email = models.EmailField(null=True, default=None, blank=True)
+    description = models.CharField(max_length=300)
+    updated_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "{} {}".format(self.first_name, self.last_name)
+
+
+
--- seed.py
+++ seed.py
@@ -10,7 +10,7 @@ django.setup()
 
 from faker import Faker
 
-from portal.models import Event, PersonStatus
+from portal.models import Event, PersonStatus, Person
 
 
 Event.objects.all().delete()
@@ -26,3 +26,54 @@ safe_status = PersonStatus(name='safe')
 safe_status.save()
 
 # to do: create 30 Person objects using Faker
+fake = Faker()
+
+def fake_first_name():
+	return fake.first_name()
+
+def fake_last_name():
+	return fake.last_name()
+
+def fake_other_name():
+	return fake.name()
+
+def fake_id_number():
+	return random.randint(1000, 100000)
+
+def fake_dob():
+	return fake.date()
+
+def fake_mobile():
+	return fake.phone_number()
+
+def fake_email():
+	return fake.email()
+
+def fake_description():
+	return fake.sentence()
+
+def generate_status():
+	statuses = PersonStatus.objects.all()
+	status = random.choice(statuses)
+	return status
+
+def generate_person(number):
+	for i in range (0, number):
+		p = Person(
+			first_name=fake_first_name(),
+			last_name=fake_last_name(),
+			status=generate_status(),
+			id_number=fake_id_number(),
+			dob=fake_dob(),
+			mobile=fake_mobile(),
+			email=fake_email(),
+			description=fake_description(),
+			)
+
+		p.save()
+
+# generate_person(30)
+
+
+
+
